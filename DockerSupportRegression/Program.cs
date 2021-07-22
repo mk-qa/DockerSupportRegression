fa97041d2366f0ed2de00a22f38f8145c7c6d475
mk <marianna.kurtukova@jetbrains.com>
2021-07-22 12:13:02 +0300

    Initial commit

---
+++ DockerSupportRegression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DockerSupportRegression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
